3cde7206b2a7b1df65721db03c5acfac869f0861
rogerio monteiro sousa <rogeriomonteiros@gmail.com>
2022-04-24 00:10:15 -0300

    test: add new test

--- Test/TestSDET.cs
+++ Test/TestSDET.cs
@@ -28,26 +28,26 @@ namespace Test
             aPIUtils.SetResponse("401", "user not authorized");
 
             var responseDownload = aPIUtils.APIJson("https://api.example.com/docs/download?id=" + DocumentFake.Document1Id, "");
-            responseDownload.StatusCode.Should().Be( ((int)StatusCode.Status401).ToString() );
+            responseDownload.StatusCode.Should().Be(((int)StatusCode.Status401).ToString());
             responseDownload.Message.Should().Be("user not authorized");
 
         }
-        
+
         [Fact]
         public void Manager_can_grant_an_employee_access_to_document()
         {
-            User user = UserFake.AllUsers().Where(x => x.IsManager ).FirstOrDefault();
+            User user = UserFake.AllUsers().Where(x => x.IsManager).FirstOrDefault();
             this.LoginAPI(user);
 
             //Employee without Document1 access
             User employee = UserFake.AllUsers().Where(x => x.Documents != null && !x.Documents.Select(b => b.Id).Contains(DocumentFake.Document1Id)).FirstOrDefault();
-            var body =  JsonSerializer.Serialize(aPIUtils.GetAccessDocumentBody(employee.Id, DocumentFake.Document1Id));
-            
+            var body = JsonSerializer.Serialize(aPIUtils.GetAccessDocumentBody(employee.Id, DocumentFake.Document1Id));
+
             //Preparing response because there is no API to test. In real test we must remove this command
             aPIUtils.SetResponse("200", "ok");
 
             var response = aPIUtils.APIJson("https://api.example.com/accesses/add", body);
-            response.StatusCode.Should().Be( ((int)StatusCode.Status200).ToString() );
+            response.StatusCode.Should().Be(((int)StatusCode.Status200).ToString());
             response.Message.Should().Be("ok");
 
             this.LoginAPI(employee);
@@ -56,6 +56,24 @@ namespace Test
             responseDownload.StatusCode.Should().Be(((int)StatusCode.Status200).ToString());
             responseDownload.Message.Should().Be("ok");
 
+        }
+
+        [Fact]
+        public void Manager_can_not_grant_access_to_another_manger()
+        {
+            User user = UserFake.AllUsers().Where(x => x.IsManager).FirstOrDefault();
+            this.LoginAPI(user);
+
+            //Find another manager
+            User manager = UserFake.AllUsers().Where(x => x.IsManager && x.Id != user.Id).FirstOrDefault();
+            var body = JsonSerializer.Serialize(aPIUtils.GetAccessDocumentBody(manager.Id, DocumentFake.Document1Id));
+
+            //Preparing response because there is no API to test. In real test we must remove this command
+            aPIUtils.SetResponse("400", "manager can not guant access to another manager");
+
+            var response = aPIUtils.APIJson("https://api.example.com/accesses/add", body);
+            response.StatusCode.Should().Be(((int)StatusCode.Status400).ToString());
+            response.Message.Should().Be("manager can not guant access to another manager");
         }
 
         private void LoginAPI(User user)
